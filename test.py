8866e8c2fd7ed483a8a553d4265e81f0824e36a6
charles <charles.nwofor1@gmail.com>
2024-11-27 00:34:03 +0100

    integration tests using unittest framework

--- test.py
+++ test.py
@@ -1,53 +1,80 @@
-import logging
+import unittest
 import boto3
 import praw
-from lambda_function import r #laziness lol
+import cred
 
-bucket_name = 'of-have-bot'
-file_name = 'comments-replied-to.txt'
+class TestIntegration(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # initialize reddit client
+        cls.r = praw.Reddit(
+            client_id=cred.client_id,
+            client_secret=cred.client_secret,
+            username=cred.username,
+            password=cred.password,
+            user_agent=cred.user_agent
+        )
 
-s3 = boto3.client('s3')
-
-handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-for logger_name in ("praw", "prawcore"):
-    logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
-
-# test that the ids of new comments are added to the list of IDs on AWS S3
-def test_persisting_ids_to_s3():
-    Ids = ['id1', 'id2', 'id3']
-
-    Ids_to_s3 = '\n'.join(Ids)
-    s3.put_object(Bucket=bucket_name, Key=file_name, Body=Ids_to_s3)
+        # initialize s3 client
+        cls.s3 = boto3.client('s3')
+        cls.bucket_name = 'of-have-bot'
+        cls.file_name = 'comments-replied-to.txt'
     
-    # get Ids of already replied-to comments from S3
-    response = s3.get_object(Bucket=bucket_name, Key=file_name)
-    existing_list = response['Body'].read().decode('utf-8').split('\n')
-    assert Ids == existing_list
+    def test_persisting_ids_to_s3(self):
+        Ids = ['id1', 'id2', 'id3']
+        Ids_to_s3 = '\n'.join(Ids)
 
-    # add new ids and persist to s3
-    updated_list_before_persist = existing_list + ['id4', 'id5']
-    new_list_of_ids = '\n'.join(updated_list_before_persist)
-    s3.put_object(Bucket=bucket_name, Key=file_name, Body=new_list_of_ids)
+        # upload initial Ids to s3
+        self.s3.put_object(Bucket=self.bucket_name, Key=self.file_name, Body=Ids_to_s3)
 
-    # get updated list from s3 and check if new
-    response = s3.get_object(Bucket=bucket_name, Key=file_name)
-    updated_list_of_ids_from_s3 = response['Body'].read().decode('utf-8').split('\n')
-    assert updated_list_before_persist == updated_list_of_ids_from_s3
+        # get list of Ids from s3 and verify
+        response = self.s3.get_object(Bucket=self.bucket_name, Key=self.file_name)
+        existing_list = response['Body'].read().decode('utf-8').split('\n')
+        self.assertEqual(Ids, existing_list)
 
-# create a new post with matching comments to test
-def test_matching_comments_from_reddit():
-    sub = 'Boxing_Clips'
-    subreddit = r.subreddit(sub)
+        # append new Ids
+        updated_list_before_persist = existing_list + ['id4', 'id5']
+        new_list_of_ids = '\n'.join(updated_list_before_persist)
+        self.s3.put_object(Bucket=self.bucket_name, Key=self.file_name, Body=new_list_of_ids)
 
-    # first clear subreddit
-    for submission in subreddit.hot():
-        submission.delete()
+        # verify updated list in s3
+        response = self.s3.get_object(Bucket=self.bucket_name, Key=self.file_name)
+        updated_list_of_ids_from_s3 = response['Body'].read().decode('utf-8').split('\n')
+        self.assertEqual(updated_list_before_persist, updated_list_of_ids_from_s3)
+    
 
-    # create a new post
-    new_post = subreddit.submit(title='Test Post', selftext=)
+    def test_read_comments_from_reddit(self):
+        sub = 'Boxing_Clips'   # subreddit was created specifically to test this bot
+        subreddit = self.r.subreddit(sub)
 
+        # load the test data
+        with open('./test_resources/submission_text.txt') as f:
+            text_for_submission = f.read()
+        
+        with open('./test_resources/comments.txt') as f_comments:
+            comment_list = f_comments.readlines()
+            
+        # clear the subreddit
+        for submission in subreddit.hot():
+            submission.delete()
 
-test_persisting_ids_to_s3()
+        # create a new post
+        new_post = subreddit.submit(title='Test Post', selftext=text_for_submission)
+
+        # add comments to the post
+        for comment in comment_list:
+            new_post.reply(comment)
+
+        # read comments from posts in subreddit
+        # TODO: Delete older posts from the subreddit
+        comment_list_from_reddit = [comment.body for post in subreddit.hot() for comment in post.comments]
+
+        # verify comments match
+        self.assertEqual(len(comment_list), len(comment_list_from_reddit))
+        self.assertListEqual(
+            sorted(comment.strip() for comment in comment_list),
+            sorted(comment.strip() for comment in comment_list_from_reddit)
+        )
+
+if __name__ == '__main__':
+    unittest.main()
